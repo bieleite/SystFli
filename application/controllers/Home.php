af7c64aedd9f1d3af1a2caa97d887e5db146f84e
bieleite <35399711+bieleite@users.noreply.github.com>
2018-07-12 22:17:22 +0200

    Initial commit

---
+++ application/controllers/Home.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->library('form_validation');
+        $this->load->helper('form');
+        date_default_timezone_set('Europe/Paris');
+    }
+    function index(){
+        redirect('login');
+    }
+}
---
+++ application/views/view_home.php
@@ -0,0 +1,1 @@
+<?php echo "Foda-se, teste " ?>
