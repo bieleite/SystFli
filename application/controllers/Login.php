04f076bf2503b6a6b018ccd4f0f2822486907e4b
Gabriel <gabriel.leiteviana@yahoo.com>
2018-08-14 15:23:46 +0200

    14-08-2018-15-23

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -2,17 +2,16 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
-    function __construct()
-    {
+
+    function __construct() {
         parent::__construct();
         $this->load->helper('url');
         $this->load->library('form_validation');
         $this->load->helper('form');
-        date_default_timezone_set('America/Sao_Paulo');
+        date_default_timezone_set('Europe/Paris');
     }
     public function index(){
         $this->load->helper(array('form'));
         $this->load->view('view_login');
     }
-    
 }
